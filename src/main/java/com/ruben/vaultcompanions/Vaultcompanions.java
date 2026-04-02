5ac24f2338161e32e2ea83d327fc142855d98c02
Ruben <unborn998@gmail.com>
2026-04-02 19:28:26 +0200

    init

--- src/main/java/com/ruben/vaultcompanions/Vaultcompanions.java
+++ src/main/java/com/ruben/vaultcompanions/Vaultcompanions.java
@@ -1,75 +1,38 @@
 package com.ruben.vaultcompanions;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
+import com.ruben.vaultcompanions.command.VaultCompanionsCommand;
+import com.ruben.vaultcompanions.handler.CompanionInteractionHandler;
+import com.ruben.vaultcompanions.network.ModNetwork;
+import com.ruben.vaultcompanions.registry.ModPets;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.RegistryEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.InterModComms;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
-import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
-import net.minecraftforge.event.server.ServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
-import java.util.stream.Collectors;
+@Mod(VaultCompanions.MOD_ID)
+public class VaultCompanions {
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("vaultcompanions")
-public class Vaultcompanions {
+    public static final String MOD_ID = "vaultcompanions";
+    public static final Logger LOGGER = LogUtils.getLogger();
 
-    // Directly reference a slf4j logger
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public Vaultcompanions() {
-        // Register the setup method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
-        // Register the enqueueIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
-        // Register the processIMC method for modloading
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::processIMC);
-
-        // Register ourselves for server and other game events we are interested in
-        MinecraftForge.EVENT_BUS.register(this);
+    public VaultCompanions() {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::onCommonSetup);
+        MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
+        MinecraftForge.EVENT_BUS.register(new CompanionInteractionHandler());
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
-        // Some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
-    }
-
-    private void enqueueIMC(final InterModEnqueueEvent event) {
-        // Some example code to dispatch IMC to another mod
-        InterModComms.sendTo("vaultcompanions", "helloworld", () -> {
-            LOGGER.info("Hello world from the MDK");
-            return "Hello world";
+    private void onCommonSetup(FMLCommonSetupEvent event) {
+        ModNetwork.register();
+        event.enqueueWork(() -> {
+            ModPets.register();
+            LOGGER.info("VaultCompanions loaded {} custom pet types", ModPets.getRegisteredTypeCount());
         });
     }
 
-    private void processIMC(final InterModProcessEvent event) {
-        // Some example code to receive and process InterModComms from other mods
-        LOGGER.info("Got IMC {}", event.getIMCStream().map(m -> m.messageSupplier().get()).collect(Collectors.toList()));
-    }
-
-    // You can use SubscribeEvent and let the Event Bus discover methods to call
-    @SubscribeEvent
-    public void onServerStarting(ServerStartingEvent event) {
-        // Do something when the server starts
-        LOGGER.info("HELLO from server starting");
-    }
-
-    // You can use EventBusSubscriber to automatically subscribe events on the contained class (this is subscribing to the MOD
-    // Event bus for receiving Registry Events)
-    @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
-    public static class RegistryEvents {
-        @SubscribeEvent
-        public static void onBlocksRegistry(final RegistryEvent.Register<Block> blockRegistryEvent) {
-            // Register a new block here
-            LOGGER.info("HELLO from Register Block");
-        }
+    private void onRegisterCommands(RegisterCommandsEvent event) {
+        VaultCompanionsCommand.register(event.getDispatcher());
     }
 }
